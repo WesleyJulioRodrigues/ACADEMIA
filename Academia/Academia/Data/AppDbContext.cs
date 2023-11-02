af88817c2caa315f319b55b55129fcc8448a29aa
Wesleyy <wesley.rodrigues63@etec.sp.gov.br>
2023-11-01 22:28:30 -0300

    01/11

--- Academia/Academia/Data/AppDbContext.cs
+++ Academia/Academia/Data/AppDbContext.cs
@@ -23,9 +23,17 @@ public class AppDbContext: IdentityDbContext
                 Id = 1,
                 Nome = "Batata Doce",
                 Descricao = "A batata-doce é considerada uma fonte de carboidrato bastante saudável, rica em fibras, proteínas, vitaminas do complexo B, vitamina A, C e diferentes sais minerais. A presença de tantas vitaminas fortalece o sistema imunológico, enquanto os sais minerais ajudam a controlar a pressão arterial. É uma fonte de carboidrato complexo de lenta absorção, fornecendo muita energia durante treinos de alta intensidade",
+                Foto = @"\imgs\"
+            },
+            new() {
+                Id = 2,
+                Nome = "Batata Doce",
+                Descricao = "A batata-doce é considerada uma fonte de carboidrato bastante saudável, rica em fibras, proteínas, vitaminas do complexo B, vitamina A, C e diferentes sais minerais. A presença de tantas vitaminas fortalece o sistema imunológico, enquanto os sais minerais ajudam a controlar a pressão arterial. É uma fonte de carboidrato complexo de lenta absorção, fornecendo muita energia durante treinos de alta intensidade",
                 Foto = @""
-            }
+            },
         };
         builder.Entity<Comida>().HasData(comidas);
+
+
     }
 }
