79ab5f47dfd241167789daf8e179eb7d933508b7
Wesleyy <wesley.rodrigues63@etec.sp.gov.br>
2023-11-08 22:10:12 -0300

    comit

--- Academia/Academia/Program.cs
+++ Academia/Academia/Program.cs
@@ -1,7 +1,16 @@
+using Academia.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+string conn = builder.Configuration.GetConnectionString("Academia");
+
+builder.Services.AddDbContext<AppDbContext>( options =>
+    options.UseMySql(conn, ServerVersion.AutoDetect(conn))
+);
 
 var app = builder.Build();
 
