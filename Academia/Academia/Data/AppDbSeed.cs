465de7b4b0f890b5744e7ca71706cd15b8a7cacf
Wesleyy <wesley.rodrigues63@etec.sp.gov.br>
2023-11-10 22:18:40 -0300

    10/11

---
+++ Academia/Academia/Data/AppDbSeed.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Academia.Models;
+
+
+public class AppDbSeed
+{
+    public AppDbSeed(ModelBuilder builder)
+    {
+
+        #region Populate Roles - Perfis de Usuário
+        List<IdentityRole> roles = new()
+        {
+            new IdentityRole()
+            {
+               Id = Guid.NewGuid().ToString(),
+               Name = "Administrador",
+               NormalizedName = "ADMINISTRADOR"
+            },
+            new IdentityRole()
+            {
+               Id = Guid.NewGuid().ToString(),
+               Name = "Funcionário",
+               NormalizedName = "FUNCIONARIO"
+            },
+            new IdentityRole()
+            {
+               Id = Guid.NewGuid().ToString(),
+               Name = "Cliente",
+               NormalizedName = "CLIENTE"
+            }
+        };
+        builder.Entity<IdentityRole>().HasData(roles);
+        #endregion
+
+        #region Populate IdentityUser
+        List<IdentityUser> users = new(){
+            new IdentityUser(){
+                Id = Guid.NewGuid().ToString(),
+                Email = "admin@academia.com",
+                NormalizedEmail = "ADMIN@ACADEMIA.COM",
+                UserName = "Admin",
+                NormalizedUserName = "ADMIN",
+                LockoutEnabled = false,
+                EmailConfirmed = true,
+            }
+        };
+
+        foreach (var user in users)
+        {
+            PasswordHasher<IdentityUser> pass = new();
+            user.PasswordHash = pass.HashPassword(user, "@Etec123");
+        }
+        builder.Entity<IdentityUser>().HasData(users);
+
+        List<Usuario> usuarios = new(){
+            new Usuario(){
+                UsuarioId = users[0].Id,
+                Nome = "Douglas",
+                Foto = "/imgs/batata doce.jpg"
+            }
+        };
+
+    }
+}
